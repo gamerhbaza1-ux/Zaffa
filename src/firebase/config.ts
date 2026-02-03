a10627ab9ab22f3e7b7be7d6df86f8cc6d3d37e8
‪Mohammed Samir (GamesYard)‬‏ <gamerhbaza1@gmail.com>
2026-02-03 17:18:55 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8949399148-35df2",
+  "appId": "1:88498255454:web:d4c04f1a81da7cade066cf",
+  "apiKey": "AIzaSyBo_DNaxVEGSxvejdT3g1u8iP765eNTPGE",
+  "authDomain": "studio-8949399148-35df2.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "88498255454"
+};
